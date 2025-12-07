a0283d9c20de7f1ef957ec62870647320eaa696e
Eniyavan-Tech <eniyavan216@gmail.com>
2025-12-07 12:37:26 +0530

    Update sensor_publish.py

--- sensor_publish.py
+++ sensor_publish.py
@@ -1,5 +1,6 @@
 import time
 import json
+import random
 import paho.mqtt.client as mqtt
 
 student_name = "Eniyavan.M"
@@ -12,13 +13,22 @@ port = 1883
 client = mqtt.Client("publisher-eniyavan")
 client.connect(broker, port)
 
+temperature = 25
+humidity = 60
+light = 300
+
 while True:
+    # simulate live sensor variation
+    temperature += random.choice([-1, 0, 1])
+    humidity += random.choice([-1, 0, 1])
+    light += random.choice([-10, 0, 10])
+
     payload = {
         "name": student_name,
         "register_no": unique_id,
-        "temperature": 25,
-        "humidity": 60,
-        "light": 300
+        "temperature": temperature,
+        "humidity": humidity,
+        "light": light
     }
 
     client.publish(topic, json.dumps(payload))
